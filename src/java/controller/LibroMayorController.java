cf618357690af74b5434ad1984a8194a4d271dc9
Sinergia003 <Sinergia003@Sinergia003-PC>
2017-04-24 15:49:05 -0600

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

--- src/java/controller/LibroMayorController.java
+++ src/java/controller/LibroMayorController.java
@@ -1,25 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.kernel.color.Color;
+import com.itextpdf.kernel.events.Event;
+import com.itextpdf.kernel.events.IEventHandler;
+import com.itextpdf.kernel.events.PdfDocumentEvent;
 import com.itextpdf.kernel.font.PdfFont;
+
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
+import static com.itextpdf.kernel.pdf.PdfName.BaseFont;
+import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.xobject.PdfFormXObject;
+import com.itextpdf.layout.Canvas;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.border.Border;
+import com.itextpdf.layout.border.SolidBorder;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.test.annotations.WrapToTest;
+import java.awt.Font;
  
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,7 +44,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LibroMayorController {
-    String dire ="/web/resources/pdf"; 
+    public static final String rut = HibernateUtil.class.getResource("/../../").getPath()+"/resources";
+   public static final String rutp =rut.replace("%20", " ");
+    public static final String fuente = rutp+"/fonts/Corbel.ttf";  
     @RequestMapping(value="libromayor.gdc")
     public ModelAndView funcLibroMayor(){
       ModelAndView mv= new ModelAndView("pgSC_libroMayor");
@@ -49,6 +62,7 @@ public class LibroMayorController {
        System.out.println(" "+fecha2);
           libroMayorDAO libro = new libroMayorDAO();
           int sociedad = Integer.parseInt((String) request.getSession().getAttribute("ses_idusuario"));
+          String sociedadnom = (String) request.getSession().getAttribute("ses_sociedadDefault");
    List cuentalibroMayor= libro.cuentaslibroMayor(fecha, fecha2, sociedad);
     ArrayList<String> Codigocuentam = new ArrayList<String>();
     ArrayList<String> nombrecuentam = new ArrayList<String>(); 
@@ -95,15 +109,181 @@ public class LibroMayorController {
             mv.addObject("saldodebe", saldodebe);
             mv.addObject("saldohaber", saldohaber);
             mv.addObject("Saldo", Saldo);*/
-      
+     //crear evento
+     
+           
+           
+           
+      // crear libro
         String ruta= HibernateUtil.class.getResource("/../../").getPath();
          System.out.println(ruta);
         ruta=ruta.replace("%20", " ");
         String rutafinal= ruta.replace("%20", " ");
-        //File path1 = new File(rutafinal+"../portalCandidato/Candidato"+idCandidato+"/"+"HOJA" + idCandidato + ".pdf");
+        System.out.println(rutafinal+"/web/resources/pdf/1.pdf");
+        PdfWriter writer = new PdfWriter(rutafinal+"resources/pdf/1.pdf");
+       
+PdfDocument pdf = new PdfDocument(writer); 
+   //Create event-handlers
+        String header = sociedadnom;
+        Header headerHandler = new Header(header);
+        PageXofY footerHandler = new PageXofY(pdf);
+  //Assign event-handlers
+        pdf.addEventHandler(PdfDocumentEvent.START_PAGE,headerHandler);
+      
+
+Document document = new Document(pdf, PageSize.LETTER);
+document.setMargins(20, 20, 20, 20);
+  // File path1 = new File(rutafinal+"../web/resources/pdf/"+sociedadnom+"-"+fecha+"-"+fecha2+".pdf");
+     //*****fuente
+        PdfFont baseFont= PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
+        
+        System.out.println("fgfghgfh");
+        System.out.println(baseFont);
+        Table table = new Table(new float[]{24, 8, 8,15,15,8, 8, 8, 8});
+        //table.setWidthPercent(100);
+     
+        table.setBorder(new SolidBorder(0));
+        document.add(new Paragraph("").setFont(baseFont));
+        document.add(new Paragraph("").setFont(baseFont));
+            table.addHeaderCell(new Cell().add(new Paragraph("FECHA").setFont(baseFont).setFontSize(7)));
+            table.addHeaderCell(new Cell().add(new Paragraph("PARTIDA").setFont(baseFont).setFontSize(7)));
+            table.addHeaderCell(new Cell().add(new Paragraph("CUENTA").setFont(baseFont).setFontSize(7)));
+            table.addHeaderCell(new Cell().add(new Paragraph("NOMBRE DE LA CUENTA").setFont(baseFont).setFontSize(7)));
+            table.addHeaderCell(new Cell().add(new Paragraph("CONCEPTO").setFont(baseFont).setFontSize(7)));
+            table.addHeaderCell(new Cell().add(new Paragraph("SALDO INICIAL").setFont(baseFont).setFontSize(7)));
+            table.addHeaderCell(new Cell().add(new Paragraph("DEBE").setFont(baseFont).setFontSize(7)));
+            table.addHeaderCell(new Cell().add(new Paragraph("HABER").setFont(baseFont).setFontSize(7)));
+            table.addHeaderCell(new Cell().add(new Paragraph("SALDO").setFont(baseFont).setFontSize(7)));
+            double saldodebev=0,tdebe=0,saldohaberv=0,thaber=0,Saldot=0;
+            DecimalFormat df = new DecimalFormat("#.00");
+        for (int i = 0; i < Codigocuentam.size()-1; i++) {  
+            System.out.println(Codigocuentam.size());
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont)).setBorder(Border.NO_BORDER).setFontSize(8));
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont)).setBorder(Border.NO_BORDER).setFontSize(8));
+            table.addCell(new Cell().add(new Paragraph(Codigocuentam.get(i)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph(nombrecuentam.get(i)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            saldodebev=0;tdebe=0;saldohaberv=0;thaber=0;Saldot=0;
+            for (int j = 0; j < fechap.size()-1; j++) {
+                String codigoh=Codigocuentam.get(i);
+                String codigod=codigocuenta.get(j);
+                
+                if (codigod.equals(codigoh)) {
+                    saldodebev=Double.parseDouble(debe.get(j));
+                    tdebe+=saldodebev;
+                    saldohaberv=Double.parseDouble( haber.get(j));
+                    thaber+=saldohaberv;
+                    Saldot+=saldodebev-saldohaberv;
+                    System.out.println(j+"sumando los debe; "+saldodebev+"Sumando los haber ; "+saldohaberv+" saldo "+Saldot );
+                table.addCell(new Cell().add(new Paragraph(fechap.get(j)).setFont(baseFont).setFontSize(6)).setBorder(Border.NO_BORDER));
+                table.addCell(new Cell().add(new Paragraph(codigopartida.get(j)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+                table.addCell(new Cell().add(new Paragraph(codigocuenta.get(j)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+                table.addCell(new Cell().add(new Paragraph(nombrecuenta.get(j)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+                table.addCell(new Cell().add(new Paragraph(concepto.get(j)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+                table.addCell(new Cell().add(new Paragraph("0").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+                table.addCell(new Cell().add(new Paragraph(debe.get(j)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+                table.addCell(new Cell().add(new Paragraph(haber.get(j)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+                table.addCell(new Cell().add(new Paragraph(String.valueOf(df.format(Saldot)).replace(',','.')).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER)); 
+                
+                
+                }
+                 }
+             double saldop=0;
+            saldop=tdebe-thaber;
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph(nombrecuentam.get(i)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph("").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph("Saldos").setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph(String.valueOf(tdebe)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph(String.valueOf(thaber)).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+            table.addCell(new Cell().add(new Paragraph(String.valueOf(df.format(saldop)).replace(',','.')).setFont(baseFont).setFontSize(7)).setBorder(Border.NO_BORDER));
+           
+            
+            System.out.println(i+" "+" "+nombrecuentam.get(i)+" "+tdebe+" "+thaber +"saldo total ; "+saldop); 
+            saldodebev=0;
+                saldohaberv=0;
+                Saldot=0; 
+               thaber=0;
+               thaber=0;
+           
+       }
+        document.add(table.setBorder(Border.NO_BORDER));
+        document.close();
+        
+
     
      System.out.println(ruta);
      System.out.println(rutafinal);
         return mv;
      }
+      //Header event handler
+    protected class Header implements IEventHandler {
+        String header;
+        public Header(String header) {
+            this.header = header;
+        }
+        @Override
+        public void handleEvent(Event event) {
+            //Retrieve document and
+            PdfDocumentEvent docEvent = (PdfDocumentEvent) event;
+            PdfDocument pdf = docEvent.getDocument();
+            PdfPage page = docEvent.getPage();
+            Rectangle pageSize = page.getPageSize();
+            PdfCanvas pdfCanvas = new PdfCanvas(
+                    page.getLastContentStream(), page.getResources(), pdf);
+            Canvas canvas = new Canvas(pdfCanvas, pdf, pageSize);
+            canvas.setFontSize(18f);
+            //Write text at position
+            canvas.showTextAligned(header,
+                    pageSize.getWidth() / 2,
+                    pageSize.getTop() - 30, TextAlignment.CENTER);
+        }
+    }
+ 
+    //page X of Y 
+    protected class PageXofY implements IEventHandler {
+        protected PdfFormXObject placeholder;
+        protected float side = 20;
+        protected float x = 300;
+        protected float y = 25;
+        protected float space = 4.5f;
+        protected float descent = 3;
+        public PageXofY(PdfDocument pdf) {
+            placeholder =
+                    new PdfFormXObject(new Rectangle(0, 0, side, side));
+        }
+        @Override
+        public void handleEvent(Event event) {
+            PdfDocumentEvent docEvent = (PdfDocumentEvent) event;
+            PdfDocument pdf = docEvent.getDocument();
+            PdfPage page = docEvent.getPage();
+            int pageNumber = pdf.getPageNumber(page);
+            Rectangle pageSize = page.getPageSize();
+            PdfCanvas pdfCanvas = new PdfCanvas(
+                    page.getLastContentStream(), page.getResources(), pdf);
+            Canvas canvas = new Canvas(pdfCanvas, pdf, pageSize);
+            Paragraph p = new Paragraph()
+                    .add("Page ").add(String.valueOf(pageNumber)).add(" of");
+            canvas.showTextAligned(p, x, y, TextAlignment.RIGHT);
+            pdfCanvas.addXObject(placeholder, x + space, y - descent);
+            pdfCanvas.release();
+        }
+        public void writeTotal(PdfDocument pdf) {
+            Canvas canvas = new Canvas(placeholder, pdf);
+            canvas.showTextAligned(String.valueOf(pdf.getNumberOfPages()),
+                    0, descent, TextAlignment.LEFT);
+        }
+
+        
+    }
+    public void onStartPage(PdfWriter writer, Document document) {
+ ColumnText.showTextAligned(writer.getDirectContent(), Element.ALIGN_CENTER, new Phrase("Inicio de Pagina - El lado oscuro de Java"),200,830,0);
+}
+ 
 }
